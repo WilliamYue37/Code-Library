5d451c31eafcc7978c8ba3e552e19a41487ebfa0
William Yue <williamyue37@gmail.com>
2020-10-18 17:26:56 -0500

    updated macros

--- C++/Dijkstra.cpp
+++ C++/Dijkstra.cpp
@@ -1,13 +1,13 @@
-vector<ll> dijkstra(int start) {
-	vector<ll> dist(N + 1, INF);
-	priority_queue<pl, vector<pl>, greater<pl>> pq;
+vector<int> dijkstra(int start) {
+	vector<int> dist(N + 1, INF);
+	priority_queue<pi, vector<pi>, greater<pi>> pq;
 	pq.push({start, 0}); //u, dist
 	dist[start] = 0;
 	while (!pq.empty()) {
-		ll u = pq.top().ff, d = pq.top().ss; pq.pop();
+		int u = pq.top().ff, d = pq.top().ss; pq.pop();
 		if (d > dist[u]) continue;
-		for (auto edge : adjList[u]) {
-			ll v = edge.ff, w = edge.ss;
+		for (auto edge : adj[u]) {
+			int v = edge.ff, w = edge.ss;
 			if (dist[u] + w < dist[v]) {
 				pq.push({v, dist[u] + w});
 				dist[v] = dist[u] + w;
