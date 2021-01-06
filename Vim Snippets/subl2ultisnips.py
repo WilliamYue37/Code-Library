4e45179f57102169f0798756c94afe8fb1115dfc
William Yue <williamyue37@gmail.com>
2021-01-06 17:59:36 -0600

    add Vim UltiSnips snippets

---
+++ Vim Snippets/subl2ultisnips.py
@@ -0,0 +1,15 @@
+import os
+
+out = open('/Users/williamyue/.vim/UltiSnips/cpp.snippets', 'w')
+for f in os.listdir('../Sublime Snippets'):
+    subl = open('../Sublime Snippets/' + f, 'r')
+    snip, trigger, descrip, flag  = '', '', '', False
+    for line in subl:
+        if ']]></content>' in line: flag = False
+        if flag:
+            snip += line + '\n'
+        if '<content><![CDATA[' in line: flag = True
+
+        if '<tabTrigger>' in line: trigger = line.split('tabTrigger')[1][1:-2]
+        if '<description>' in line: descrip = line.split('description')[1][1:-2]
+    out.write('snippet ' + trigger + ' \"' + descrip + '\"\n' + snip[:-1] + 'endsnippet\n\n')
